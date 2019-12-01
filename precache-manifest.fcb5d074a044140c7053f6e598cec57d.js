b11e279f78f84f59b018001181431b7a8854049e
ashisbanerjee <ashisbanerjee325@gmail.com>
2019-12-01 19:41:23 +0530

    Updates

---
+++ precache-manifest.fcb5d074a044140c7053f6e598cec57d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8c14cdf9ad2d93b487ccb266a64d82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "063a4e88e975457d4b7a",
+    "url": "/monsters-rolodex/static/css/main.80fdd42e.chunk.css"
+  },
+  {
+    "revision": "46248e6a53b0406f2c95",
+    "url": "/monsters-rolodex/static/js/2.3d96d2f3.chunk.js"
+  },
+  {
+    "revision": "063a4e88e975457d4b7a",
+    "url": "/monsters-rolodex/static/js/main.51d99ee4.chunk.js"
+  },
+  {
+    "revision": "526815fe5610cbb3528e",
+    "url": "/monsters-rolodex/static/js/runtime-main.b373d6e7.js"
+  }
+]);
